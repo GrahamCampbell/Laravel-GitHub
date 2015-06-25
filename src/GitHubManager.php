e899f4b3f22fd9a9e5af755db2b214fc8466dbfd
Graham Campbell <graham@alt-three.com>
2015-06-25 14:37:57 +0100

    Updated email

--- src/GitHubManager.php
+++ src/GitHubManager.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel GitHub.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -55,7 +55,7 @@ use Illuminate\Contracts\Config\Repository;
  * @method void setOption(string $name, mixed $value)
  * @method array getSupportedApiVersions()
  *
- * @author Graham Campbell <graham@cachethq.io>
+ * @author Graham Campbell <graham@alt-three.com>
  */
 class GitHubManager extends AbstractManager
 {
