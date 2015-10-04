f530d58d1d6d53631e1908f371315d48fd93416d
Graham Campbell <graham@alt-three.com>
2015-10-04 11:50:44 +0100

    Fixed lumen configuration

--- src/GitHubServiceProvider.php
+++ src/GitHubServiceProvider.php
@@ -30,22 +30,24 @@ class GitHubServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig();
+        $this->setupConfig($this->app);
     }
 
     /**
      * Setup the config.
      *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
      * @return void
      */
-    protected function setupConfig()
+    protected function setupConfig(Application $app)
     {
         $source = realpath(__DIR__.'/../config/github.php');
 
         if (class_exists('Illuminate\Foundation\Application', false)) {
             $this->publishes([$source => config_path('github.php')]);
         } elseif (class_exists('Laravel\Lumen\Application', false)) {
-            $this->configure('github');
+            $app->configure('github');
         }
 
         $this->mergeConfigFrom($source, 'github');
