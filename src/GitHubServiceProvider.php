05efd58cbd75aabd1ec20021a471ba3a0d76ae48
Graham Campbell <graham@alt-three.com>
2015-11-08 19:58:01 +0000

    Improved environment specific code

--- src/GitHubServiceProvider.php
+++ src/GitHubServiceProvider.php
@@ -14,7 +14,9 @@ namespace GrahamCampbell\GitHub;
 use Github\Client;
 use GrahamCampbell\GitHub\Authenticators\AuthenticatorFactory;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the github service provider class.
@@ -44,9 +46,9 @@ class GitHubServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/github.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('github.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('github');
         }
 
