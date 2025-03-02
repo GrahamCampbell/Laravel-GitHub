311d9e1c15dfed22b1c011aa40784a3eebe8ad7d
Graham Campbell <hello@gjcampbell.co.uk>
2025-03-02 22:27:19 +0000

    PHP 8.4 compat fixes

--- src/GitHubManager.php
+++ src/GitHubManager.php
@@ -134,7 +134,7 @@ class GitHubManager extends AbstractManager
      *
      * @return array
      */
-    public function getConnectionConfig(string $name = null): array
+    public function getConnectionConfig(?string $name = null): array
     {
         $config = parent::getConnectionConfig($name);
 
